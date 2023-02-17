cbc9cee7082b1e15aa108438de17cdda1c56aa3c
Emerson <emerson.day@gmail.com>
2023-02-16 19:59:54 -0800

    touch up parallax

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,10 +10,10 @@ function Hero() {
       <Parallax
         className='bg-zinc-50'
         style={{ top: '0', left: '0' }}
-        pages={4}
+        pages={3}
       >
         <ParallaxLayer
-          className='min-h-screen bg-gradient-to-tr from-slate-800 via-slate-900 to-zinc-800 flex justify-center items-center'
+          className='border-2 border-pink-900 min-h-screen bg-gradient-to-tr from-slate-800 via-slate-900 to-zinc-800 flex justify-center items-center'
           offset={0}
           speed={0.5}
         >
@@ -26,15 +26,15 @@ function Hero() {
             alt='logo'
           ></Image>
         </ParallaxLayer>
-        <ParallaxLayer offset={0.999} speed={1}>
-          <div className='max-w-7xl z-10 mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between bg-orange-500'>
+        <ParallaxLayer offset={0.999} speed={5}>
+          <div className='max-w-7xl z-10 mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:justify-between lg:items-center bg-orange-500'>
             <h2 className='text-3xl p-6 font-extrabold tracking-tight text-slate-900 sm:text-4xl'>
-              <span className='block'>Want to catch bigger fish?</span>
+              <span className=''>Want to catch bigger fish?</span>
             </h2>
           </div>
         </ParallaxLayer>
         <ParallaxLayer
-          className='bg-zinc-50 flex flex-col justify-center items-center'
+          className='bg-zinc-50 border border-red-700 flex flex-col justify-center items-center'
           offset={0.9999}
           speed={0.2}
         >
@@ -47,19 +47,29 @@ function Hero() {
               {' '}
               <span className=' text-slate-100'>Perko Lures</span>
             </h2>
-            <p className='pb-8 lg:px-48 md:text-xl px-2 md:px-24'>
+            <p className='pb-8 lg:px-48 md:text-xl lg:text-2xl px-2 md:px-24'>
               Handcrafted, extra large fishing lures designed to catch the
-              biggest freshwater fish. Each lure is carefully crafted to
-              perfection, ensuring that it is not only effective but also
-              visually appealing to attract the biggest fish. Perko Lures is
-              dedicated to providing anglers with the best tools to make their
-              fishing experience both exciting and successful.
+              biggest fish. Each lure is carefully crafted to perfection,
+              ensuring that it is not only effective but also visually appealing
+              to attract the biggest fish. Perko Lures is dedicated to providing
+              anglers with the best tools to make their fishing experience both
+              exciting and successful.
             </p>
           </div>
         </ParallaxLayer>
 
         <ParallaxLayer className='bg-zinc-50' offset={2} speed={0.1}>
           <PictureGrid />
+        </ParallaxLayer>
+        <ParallaxLayer offset={2.5} speed={3} className='bg-slate-800 pt-5'>
+          <div className='max-w-xl z-10 mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between bg-orange-500 rounded shadow-2xl'>
+            <h2 className='text-3xl text-center p-6 font-extrabold tracking-tight text-slate-900 sm:text-4xl'>
+              <span className='text-center'>CHECK OUT OUR LURES</span>
+            </h2>
+          </div>
+          <p className='text-center pt-12 text-slate-50'>
+            &copy; {new Date().getFullYear()} Perko Lures. All rights reserved.
+          </p>
         </ParallaxLayer>
       </Parallax>
     </div>
