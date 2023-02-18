60ef7467293628dabfa57895da1f6b8f2d4bcf7c
Emerson <emerson.day@gmail.com>
2023-02-17 21:32:29 -0800

    removed img bgs

--- app/lures/page.tsx
+++ app/lures/page.tsx
@@ -7,14 +7,32 @@ function Lures() {
       <h1>Lures</h1>
       <p>Here you can find all the lures you need.</p>
 
-      <CldImage
-        src='https://res.cloudinary.com/dfyd1vtup/image/upload/v1676591213/perko/Hitman_set_xb7w0d.jpg'
-        alt=''
-        width={250}
-        height={250}
-      />
-
-      <div className='max-w-6xl grid gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  '></div>
+      <div className='max-w-6xl grid gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  '>
+        <div>
+          <CldImage
+            src='https://res.cloudinary.com/dfyd1vtup/image/upload/v1676697860/perko/Grid_search_set-removebg_jqedsp.png'
+            alt=''
+            width={250}
+            height={250}
+          />
+        </div>
+        <div>
+          <CldImage
+            src='https://res.cloudinary.com/dfyd1vtup/image/upload/v1676698013/perko/Hitman_set-removebg-preview_yegvpd.png'
+            alt=''
+            width={250}
+            height={250}
+          />
+        </div>
+        <div>
+          <CldImage
+            src='https://res.cloudinary.com/dfyd1vtup/image/upload/v1676591194/perko/Pick_Pocket_q4z92z.jpg'
+            alt=''
+            width={250}
+            height={250}
+          />
+        </div>
+      </div>
     </div>
   );
 }
